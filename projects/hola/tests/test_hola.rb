aa0e9959f18d37f76a3aa438c1466343f03eb69f
Ruan Odendaal <ruanodendaal@gmail.com>
2017-01-25 14:41:42 +0000

    Corrected naming in test_hola.rb

--- projects/hola/tests/test_hola.rb
+++ projects/hola/tests/test_hola.rb
@@ -1,11 +1,7 @@
 require "./lib/hola.rb"
 require "test/unit"
 
-class TestNAME < Test::Unit::TestCase
-
-  def test_sample
-    assert_equal(4, 2+2)
-  end
+class TestHola < Test::Unit::TestCase
 
   def test_any_hello
     assert_equal "hello world", Hola.hi("ruby")
