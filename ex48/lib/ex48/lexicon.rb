025a6cad5ddc4ddfdc88fbdf58117c0ca8e82c45
Ruan Odendaal <ruanodendaal@gmail.com>
2017-01-26 10:50:46 +0000

    First successful attempt at making the tests pass

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -3,27 +3,37 @@
 # of an array of arrays with the (TOKEN, WORD)
 # pairings. If a word isn't part of the lexicon
 # then is should still return the WORD but set
-# the TOKEN to an error token.
+# the TOKEN to an error token. #TODO
 class Lexicon
 
 @@dictionary = {
   'direction' => ['north', 'south', 'east'],
-  'verb' => ['go', 'kill', 'eat']
+  'verb' => ['go', 'kill', 'eat'],
+  'stop' => ['the', 'in', 'of'],
+  'noun' => ['bear', 'princess'],
+  'error' => []
 }
 
   def self.scan(str)
     sentence = []
     str.split.each { |str|
       pairing = []
-      @@dictionary.each { |token, word|
-        if word.include?(str.downcase)
-          pairing = [token, str]
-          sentence.push(pairing)
-        end
-      }
+      if str =~ /\d/ #TODO - doesn't like numbers yet
+        pairing = ['number', str.to_i]
+        sentence.push(pairing)
+      else
+        @@dictionary.each { |token, word|
+          if word.include?(str.downcase)
+            pairing = [token, str.downcase]
+            sentence.push(pairing)
+            break
+          elsif @@dictionary[token].empty?
+            pairing = [token, str]
+            sentence.push(pairing)
+          end
+        }
+      end
     }
-
     sentence
-
   end
 end
